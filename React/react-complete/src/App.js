40015449204e49ffe090d2db4801c4b1074be609
Olivia Kumar <oliviakumar@gmail.com>
2019-06-05 11:07:47 -0700

    binding

--- React/react-complete/src/App.js
+++ React/react-complete/src/App.js
@@ -1,54 +1,75 @@
                 // hook is used to manage state in functional component
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React, { Component } from 'react';
+
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
+class App extends Component {
+// const app = props => {
   // returns array with two elems
     // gives access to persons object   // function that allows you to set person's state
-  const [ personsState, setPersonsState ] = useState({ // adds state management to functional components
+  // const [ personsState, setPersonsState ] = useState({ // adds state management to functional components
+  //   persons: [
+  //     { name: "Olivia", age: 25},
+  //     { name: "Asher", age: 26},
+  //     { name: "Cathy", age: 24}
+  //   ],
+  //   otherState: 'other val'
+  // })
+
+  state = {
     persons: [
       { name: "Olivia", age: 25},
       { name: "Asher", age: 26},
       { name: "Cathy", age: 24}
     ],
     otherState: 'other val'
-  });
+  };
+  // const [otherState, setOtherState] = useState('some other val');
 
-  const [otherState, setOtherState] = useState('some other val');
+  // console.log(personsState, otherState);
 
-  console.log(personsState, otherState);
-
-  const switchNameHandler = () => {
-    console.log("was clicked!");
-    // this.setState({
-    setPersonsState({
+  // const switchNameHandler = () => {
+  //   console.log("was clicked!");
+  //   // this.setState({
+  //   setPersonsState({
+  //     persons: [
+  //       { name: "Liv", age: 25},
+  //       { name: "Sherry", age: 27},
+  //       { name: "Cathy", age: 24}
+  //     ],
+  //     otherState: personsState.otherState
+  //   });
+  // };
+  switchNameHandler = (newName) => {
+    this.setState({
       persons: [
         { name: "Liv", age: 25},
         { name: "Sherry", age: 27},
-        { name: "Cathy", age: 24}
-      ],
-      otherState: personsState.otherState
-    });
-  };
+        { name: newName, age: 24}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1> React App </h1>
         <p>Hi</p>
                         {/* // not inside class so don't need this keyword */}
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.switchNameHandler.bind(this, 'Olivia Kumar')}>Switch Name</button>
                         {/* // personsState is functional component */}
         <Person
-          name={personsState.persons[0].name}
-          age={personsState.persons[0].age}> What is happening? </Person>
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}> What is happening? </Person>
         <Person
-          name={personsState.persons[1].name}
-          age={personsState.persons[1].age}
-          click={this.switchNameHandler}> What is happening? </Person>
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Catherine')}> What is happening? </Person>
         <Person
-          name={personsState.persons[2].name}
-          age={personsState.persons[2].age}> What is happening? </Person>
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}> What is happening? </Person>
         {/* <Person name={this.state.persons[2].name}> What is happening? </Person> */}
         {/* only exists in class based react components */}
       </div>
@@ -60,8 +81,8 @@ const app = props => {
   }
 }
 
-// export default App;
-export default app;
+export default App;
+// export default app;
 
 // state = ;
 
