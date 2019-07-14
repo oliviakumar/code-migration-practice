9f56ecf1b9310d7ce3883fecde41977d49ef9192
Olivia Kumar <olivia.kumar@unity3d.com>
2019-07-13 17:57:34 -0700

    own

--- React/react-complete/src/components/Persons/Person/Person.js
+++ React/react-complete/src/components/Persons/Person/Person.js
@@ -2,17 +2,10 @@ import React from 'react';
 import './Person.css';
 
 const person = (props) => {
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    };
-
     return (
-        <div className="Person" style={style}>
-            <p onClick={props.click}> I am {props.name} and I am {props.age} years old.</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
+        <div>
+            <p> {props.name} is {props.height} cm tall. </p>
+            <p style={{fontSize: 5}}> {props.children} </p>
         </div>
     )
 };
--- React/react-complete/src/containers/App.js
+++ React/react-complete/src/containers/App.js
@@ -6,114 +6,38 @@ import Persons from '../components/Persons/Persons';
 class App extends Component {
   state = {
     persons: [
-      { id: '', name: "Olivia", age: 25},
-      { id: '', name: "Asher", age: 26},
-      { id: '', name: "Cathy", age: 24}
-    ],
-    otherState: 'other val',
-    showPersons: false
-  };
-
-  // switchNameHandler = (newName) => {
-  //   this.setState({
-  //     persons: [
-  //       { name: "Liv", age: 25},
-  //       { name: "Sherry", age: 27},
-  //       { name: newName, age: 24}
-  //     ]
-  //   })
-  // }
-
-  nameChangedHandler = (event, id) => {
-    // this.setState({
-    //   persons: [
-    //     { name: "Liv", age: 25},
-    //     { name: "Sherry", age: 27},
-    //     { name: event.target.value, age: 24}
-    //   ]
-    // })
-    const personIndex = this.state.persons.findIndex( p => {
-      return p.id == id;
-    });
-
-    const person = {
-      ...this.state.persons[personIndex]
-    };
-
-    person.name = event.target.value;
-    
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
-    this.setState({persons:persons});
+      {name: 'olivia', height: '62'},
+      {name: 'nicole', height: '60'},
+      {name: 'hannah', height: '69'},
+    ]
   }
+  handleClick(i) {
+    const persons = {...this.state.persons};
+    persons[i].name = 'liv';
+    this.setState({...persons});
     
-
-  deletePersonHandler = (personIndex) => {
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons});
+    // this.setState({persons: [//...this.state.persons,
+    //   // this.state.persons[i]:
+    //   ...{name: 'liv', height: '62'}
+    //   // {name: 'nicole', height: '60'},
+    //   // {name: 'hannah', height: '69'},
+    // ]});
   }
-
-  togglePersonHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
-  };
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      bonder: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
-    let persons = null;
-    let btnClass = '';
-
-    if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={( event ) => this.nameChangedHandler(event, person.id)} />
-          })}
-          {/* <Persons
-            persons={this.state.persons}
-            clicked={this.deletePersonHandler}
-            changed={this.nameChangedHandler}
-          /> */}
-        </div>
-      );
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      };
-    }
-
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red);//'red');
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold);//'bold');
-    }
-
     return (
       <div className="App">
         <h1> React App </h1>
-        <p className={assignedClasses.join(' ')}>Will the real slim shady please stand up?</p>
-
-        <button
-        style={style}
-        onClick={this.togglePersonHandler}>Switch Name</button>
-        {persons}
+        <button onClick={() => this.handleClick(0)}> switch name </button>
+        {this.state.persons.map((p, i) => {
+          if (i === 2) {
+            return <Person key={i} name={p.name} height={p.height}> tee hee 69 </Person>;
+          } else
+            return <Person key={i} name={p.name} height={p.height}/>;
+        })}
+        {/* {this.state.persons[0].name} */}
+        {/* <Person name="nicole" height="60"/> */}
+        {/* <Person name="hannah" height="69"> tee hee 69 </Person> */}
       </div>
     );
   }
